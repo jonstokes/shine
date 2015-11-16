6fa5ac4c32b2faf320f56c2738852a2f4c6451b2
Jon Stokes <jon@jonstokes.com>
2015-11-15 20:28:16 -0600

    Flesh out post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,16 +3,23 @@ class Post < ActiveRecord::Base
 
   def self.create_or_update_from_entry(entry)
     if post = Post.find_by(contentful_id: entry.id)
-      update_from_entry(post: post, entry: entry)
+      post.update(attrs_from_entry(entry))
     else
-      create_from_entry(entry)
+      create(attrs_from_entry(entry))
     end
   end
 
-  def self.create_from_entry(entry)
-    Post.create(
-      title: entry.fields[:title]
-      author_ids: entry.fields[:author].map(&:id)
-    )
+  def self.attrs_from_entry(entry)
+    {
+      title:          entry.fields[:title],
+      slug:           entry.fields[:slug],
+      author:         convert_author(entry.fields[:author]),
+      body:           entry.fields[:body],
+      category:       convert_category(entry.fields[:category]),
+      tags:           entry.fields[:tags],
+      featured_image: convert_asset(entry.fields[:featured_image]),
+      date:           entry.fields[:date],
+      comments:       entry.fields[:comments]
+    }
   end
 end
