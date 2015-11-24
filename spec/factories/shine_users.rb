8df19df006e48982a5df0ed59a9e193b0ee0d4cf
Jon Stokes <jon@jonstokes.com>
2015-11-24 14:30:19 -0600

    Started spesc

--- spec/factories/shine_users.rb
+++ spec/factories/shine_users.rb
@@ -1,6 +1,14 @@
 FactoryGirl.define do
-  factory :shine_user, :class => 'Shine::User' do
-    
+  factory :shine_user, class: 'Shine::User' do
+    sequence(:email) { |n| "user#{n}@example.com" }
+    sequence(:encrypted_password) { |n| "password#{n}" }
+    role { 'editor' }
+    sequence(:biography) { |n| "Biography #{n}" }
+  end
+
+  factory :shine_asset, class: 'Shine::asset' do
+    user { create(:user) }
+    sequence(:title) { |n| "Title #{n}" }
   end
 
 end
