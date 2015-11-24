5c0554ac3743fd48c4974c1a7b7f466c5ff0fce0
Jon Stokes <jon@jonstokes.com>
2015-11-24 16:03:44 -0600

    Styling

--- app/helpers/shine/posts_helper.rb
+++ app/helpers/shine/posts_helper.rb
@@ -1,4 +1,12 @@
 module Shine
   module PostsHelper
+    def row_class(post)
+      case post.status
+      when "pending"
+        "class=success"
+      when "draft"
+        "class=warning"
+      end
+    end
   end
 end
