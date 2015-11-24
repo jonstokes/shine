0f05cc03c38d84625f357e4c8aac8cff84b19bb1
Jon Stokes <jon@jonstokes.com>
2015-11-23 18:13:05 -0600

    Fixes

--- app/assets/javascripts/shine/application.js
+++ app/assets/javascripts/shine/application.js
@@ -15,5 +15,4 @@
 //= require jquery_ujs
 //= require react
 //= require react_ujs
-//= require components
 //= require_tree .
--- lib/shine.rb
+++ lib/shine.rb
@@ -7,6 +7,7 @@ require "font-awesome-sass"
 require 'bootswatch-rails'
 require 'bootstrap-sass'
 require 'bh'
+require 'react-rails'
 
 module Shine
   UUID_REGEXP = /[a-f0-9]{8}-[a-f0-9]{4}-4[a-f0-9]{3}-[89aAbB][a-f0-9]{3}-[a-f0-9]{12}/i
