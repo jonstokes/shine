20a9d3adedbaab53e60e0a14edb72e75a8c532c8
Jon Stokes <jon@jonstokes.com>
2015-11-24 14:58:22 -0600

    Some relation specs

--- spec/models/shine/category_spec.rb
+++ spec/models/shine/category_spec.rb
@@ -11,4 +11,18 @@ describe Shine::Category do
       expect(subject).not_to be_valid
     end
   end
+
+  context "relations" do
+    it "has multiple posts" do
+      category1 = create(:category)
+      category2 = create(:category)
+
+      create(:post, category_ids: [category1.id, category2.id])
+      create(:post, category_ids: [category1.id, category2.id])
+      create(:post, category_ids: [category1.id])
+
+      expect(category2.posts.count).to eq(2)
+      expect(category1.posts.count).to eq(3)      
+    end
+  end
 end
--- spec/models/shine/post_spec.rb
+++ spec/models/shine/post_spec.rb
@@ -21,4 +21,17 @@ describe Shine::Post do
       expect(subject).not_to be_valid
     end
   end
+
+  context "relations" do
+    describe "users" do
+      let(:user1) { create(:user) }
+      let(:user2) { create(:user) }
+
+      it "has multiple users" do
+        post = create(:post, user_ids: [user1.id, user2.id])
+        expect(post.users.first).to eq(user2)
+        expect(post.users.last).to eq(user1)
+      end
+    end
+  end
 end
